840c22a894f21147869fe3eebc658ff33175af5b
brian-mutsetsa <brianmutsetsa@gmail.com>
2025-03-05 19:25:14 +0200

    Completed mean-variance-standard-deviation calculator

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import mean_var_std
 from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+#changing the input to see if the output is different.
+print(mean_var_std.calculate([1,2,3,4,5,6,7,8,9]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
